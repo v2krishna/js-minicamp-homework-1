06f54e5a5aa38df58889b2ec95e90fdf71dcc37a
Kris V <v2krishna@gmail.com>
2017-03-21 22:31:54 -0500

    str with exclamation function changed

--- exercises.js
+++ exercises.js
@@ -163,6 +163,8 @@ function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
   //code here
+  var res = str +'!';
+  return res;
 }
 
 function combineNames(firstName, lastName) {
